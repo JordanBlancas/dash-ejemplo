69d28cef8294da1ccebe07e61fa01af695e2d3f6
Jordan Blancas <54956790+JordanBlancas@users.noreply.github.com>
2025-05-18 17:52:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def update_plot(amplitud):
     return fig
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=False)
 
 
